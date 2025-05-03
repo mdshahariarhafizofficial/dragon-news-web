0423d8791ac98c942135f9be584b2e1f31d7220b
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-05-03 20:31:50 +0600

    News Details Page responsive  Added

--- src/Pages/NewsDetails.jsx
+++ src/Pages/NewsDetails.jsx
@@ -17,15 +17,15 @@ const NewsDetails = () => {
 
     return (
         <div>
-            <header className='max-w-[1280px] mx-auto'>
+            <header className='max-w-[1280px] mx-auto px-5 md:px-0'>
                 <Header></Header>
             </header>
             <main className='max-w-[1280px] mx-auto my-10 grid gap-6 grid-cols-12 px-5 lg:px-0'>
-                <section className='col-span-9'>
+                <section className=' col-span-12 md:col-span-9'>
                     <h2 className='text-2xl font-bold mb-5'>News Details</h2>
                     <NewsDetailsCard news={news}></NewsDetailsCard>
                 </section>
-                <aside className='sticky top-3 h-fit col-span-3'>
+                <aside className='sticky top-3 h-fit col-span-12 md:col-span-3'>
                     <RightAside></RightAside>
                 </aside>
             </main>
